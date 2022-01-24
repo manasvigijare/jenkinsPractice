7f1a87a8d0d21aae9fcd719b517720d6fd207c50
manasvigijare <manasvigijare@gmail.com>
2022-01-24 18:42:54 +0530

    first message

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(checkEvenOdd(123));
+	}
+
+	public static boolean checkEvenOdd(int number)
+	{
+		return number%2==0;
+			
+			
+		
+		
+	}
+
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class TestMain {
+	
+	@Test
+	public void testInputEven ()
+	{
+		assertTrue (Main.checkEvenOdd(22));
+	}
+
+	
+		
+	}
+
+
